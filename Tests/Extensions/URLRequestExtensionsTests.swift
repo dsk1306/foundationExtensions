6d7d68a36210d36e375365b38a9368205280e23c
Dmitry Kuznetsov <dsk1306@icloud.com>
2023-04-07 02:05:26 +0200

    Added URLRequest extensions and extensions tests

---
+++ Tests/Extensions/URLRequestExtensionsTests.swift
@@ -0,0 +1,40 @@
+@testable import FoundationExtensions
+import XCTest
+
+final class URLRequestExtensionsTests: XCTestCase {
+
+    // MARK: - Tests
+
+    func test_appendingHeaders() {
+        let request = URLRequest(url: Mock.url)
+            .appendingHeader(value: Mock.value1, forKey: Mock.key1)
+
+        XCTAssertEqual(request.allHTTPHeaderFields, Mock.headers1)
+    }
+
+    func test_appendingHeadersArray() {
+        let request = URLRequest(url: Mock.url)
+            .appendingHeaders(Mock.headers2)
+
+        XCTAssertEqual(request.allHTTPHeaderFields, Mock.headers2)
+    }
+
+}
+
+// MARK: - Mock
+
+private extension URLRequestExtensionsTests {
+
+    enum Mock {
+
+        static let url = URL(string: "example.com")!
+        static let value1 = "Accept-Encoding"
+        static let value2 = "Accept"
+        static let key1 = "gzip, deflate, br"
+        static let key2 = "*/*"
+        static let headers1 = [key1: value1]
+        static let headers2 = [key1: value1, key2: value2]
+
+    }
+
+}
