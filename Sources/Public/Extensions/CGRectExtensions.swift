aef973f501de0adc84845b2ce316373d849fb6c0
Dmitry Kuznetsov <dsk1306@icloud.com>
2023-04-27 12:20:01 +0200

    Added CGRect extensions

---
+++ Sources/Public/Extensions/CGRectExtensions.swift
@@ -0,0 +1,30 @@
+import CoreGraphics
+import Foundation
+
+public extension CGRect {
+
+    var topLeftCorner: CGPoint {
+        .init(x: minX, y: minY)
+    }
+
+    var topMid: CGPoint {
+        .init(x: midX, y: minY)
+    }
+
+    var topRightCorner: CGPoint {
+        .init(x: maxX, y: minY)
+    }
+
+    var bottomLeftCorner: CGPoint {
+        .init(x: minX, y: maxY)
+    }
+
+    var bottomMid: CGPoint {
+        .init(x: midX, y: maxY)
+    }
+
+    var bottomRightCorner: CGPoint {
+        .init(x: maxX, y: maxY)
+    }
+
+}
